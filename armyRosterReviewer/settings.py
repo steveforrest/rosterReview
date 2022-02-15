0f2b4c61e07395991ff6a0bfb228616778db70a2
steveforrest <steveforrest_1@hotmail.com>
2022-02-15 15:38:11 +0000

    test setting debug top deploy project

--- armyRosterReviewer/settings.py
+++ armyRosterReviewer/settings.py
@@ -29,8 +29,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = 'DEVELOPEMENT' in os.environ
+DEBUG = True
+# DEBUG = 'DEVELOPEMENT' in os.environ
 
 ALLOWED_HOSTS = ['army-roster.herokuapp.com', 'localhost']
 
