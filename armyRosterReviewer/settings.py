36b84ea63978afcedb29b1e1de3640bae824edd0
steveforrest <steveforrest_1@hotmail.com>
2022-02-17 14:09:28 +0000

    using font awesome kit now

--- armyRosterReviewer/settings.py
+++ armyRosterReviewer/settings.py
@@ -29,8 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = 'DEVELOPEMENT' in os.environ
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['army-roster.herokuapp.com', 'localhost']
 
