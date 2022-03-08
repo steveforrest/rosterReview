518bf64655aa87edeb733d1143e3922affb4ad0f
steveforrest <steveforrest_1@hotmail.com>
2022-03-08 17:54:52 +0000

    moved media files

--- armyRosterReviewer/settings.py
+++ armyRosterReviewer/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
+    'cloudinary_storage',
     'roster',
     'django_summernote',
     'crispy_forms',
