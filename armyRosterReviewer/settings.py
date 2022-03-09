9d6bf3b8236388e736f7ab9f8331e641918739b1
steveforrest <steveforrest_1@hotmail.com>
2022-03-09 10:09:19 +0000

    changed url to match cloudinary to test if this resolves issue

--- armyRosterReviewer/settings.py
+++ armyRosterReviewer/settings.py
@@ -107,17 +107,18 @@ WSGI_APPLICATION = 'armyRosterReviewer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEVELOPMENT' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
 
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
