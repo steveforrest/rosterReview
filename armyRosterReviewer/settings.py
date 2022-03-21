b805bc6b86b3a3894affdf7b3ad60e6f43b8193c
steveforrest <steveforrest_1@hotmail.com>
2022-03-21 13:46:47 +0000

    made improvements from feedback

--- armyRosterReviewer/settings.py
+++ armyRosterReviewer/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = 'DEVELOPEMENT' in os.environ
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
@@ -114,7 +114,6 @@ if 'DEVELOPMENT' in os.environ:
         }
     }
 else:
-    print('loading postgres....')
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
